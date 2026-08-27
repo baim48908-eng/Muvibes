3bf13239510b3e765521fbd82973b871e209129d
baim48908-eng <baim48908@gmail.com>
2026-08-27 13:26:22 +0700

    Enhance yt-dlp process for Linux with parameters
    Updated the yt-dlp process call to include additional parameters for Linux environments.

--- app/Http/Controllers/MusicController.php
+++ app/Http/Controllers/MusicController.php
@@ -93,11 +93,18 @@ class MusicController extends Controller
         $localWindowsPath = 'D:\\laragon\\bin\\python\\python-3.10\\python.exe';
 
         try {
-            // Jika di Windows (Laragon), pakai path lokal. Jika di Linux (Railway), jalankan 'yt-dlp' langsung.
+            // Jika di Windows (Laragon), pakai path lokal. Jika di Linux (Railway), jalankan 'yt-dlp' dengan parameter bypass bot & nodejs.
             if (file_exists($localWindowsPath)) {
                 $process = new Process([$localWindowsPath, '-m', 'yt_dlp', '-g', '-f', 'bestaudio', $searchQuery]);
             } else {
-                $process = new Process(['yt-dlp', '-g', '-f', 'bestaudio', $searchQuery]);
+                $process = new Process([
+                    'yt-dlp', 
+                    '--js-runtimes', 'node',
+                    '--extractor-args', 'youtube:player-client=android,web',
+                    '-g', 
+                    '-f', 'bestaudio', 
+                    $searchQuery
+                ]);
             }
             
             $process->setTimeout(30);
