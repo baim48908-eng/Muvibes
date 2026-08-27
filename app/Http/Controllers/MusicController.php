b51e320e9e8ae759b295fe65ab6de81384086693
baim48908-eng <baim48908@gmail.com>
2026-08-27 14:18:57 +0700

    Update extractor args for yt-dlp in MusicController

--- app/Http/Controllers/MusicController.php
+++ app/Http/Controllers/MusicController.php
@@ -93,14 +93,14 @@ class MusicController extends Controller
         $localWindowsPath = 'D:\\laragon\\bin\\python\\python-3.10\\python.exe';
 
         try {
-            // Jika di Windows (Laragon), pakai path lokal. Jika di Linux (Railway), gunakan client android untuk bypass bot cloud.
+            // Jika di Windows (Laragon), pakai path lokal. Jika di Linux (Railway), gunakan client web,ios untuk bypass SABR.
             if (file_exists($localWindowsPath)) {
                 $process = new Process([$localWindowsPath, '-m', 'yt_dlp', '-g', '-f', 'bestaudio', $searchQuery]);
             } else {
                 $process = new Process([
                     'yt-dlp', 
                     '--remote-components', 'ejs:github',
-                    '--extractor-args', 'youtube:player-client=android',
+                    '--extractor-args', 'youtube:player-client=web,ios',
                     '-g', 
                     '-f', 'bestaudio', 
                     $searchQuery
